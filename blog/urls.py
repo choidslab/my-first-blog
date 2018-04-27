15679b87cc3aa5e6f67989e5411a761649f59e08
dschoi <cds0915@naver.com>
2018-04-27 15:34:33 +0900

    Added Comments

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     re_path(r'^draft/$', views.post_draft_list, name='post_draft_list'),
     re_path(r'^post/(?P<pk>\d+)/publish/$', views.post_publsh, name='post_publish'),
     re_path(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    re_path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+
 ]
